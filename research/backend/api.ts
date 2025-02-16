ba92559bebe705bacd4b1c4184a2a5740164a60c
Jean Cheng <106690804+jgchengunc@users.noreply.github.com>
2025-02-15 22:47:56 -0500

    Admin files

--- research/backend/api.ts
+++ research/backend/api.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import db from './db'; 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json()); 
